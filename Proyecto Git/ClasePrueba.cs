c57112b6d09e162f0db2c10913899acf206e687a
SebastianYui <barragansebastian129@gmail.com>
2026-04-23 15:53:04 -0500

    Ejercicios de git normales

---
+++ Proyecto Git/ClasePrueba.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class ClasePrueba
+{
+    static void Main()
+    {
+        int[] notas = { 3, 4, 2, 5, 1, 4, 3 };
+
+        int suma = 0;
+        int aprobados = 0;
+        int reprobados = 0;
+
+        for (int i = 0; i < notas.Length; i++)
+        {
+            suma += notas[i];
+
+            if (notas[i] >= 3)
+                aprobados++;
+            else
+                reprobados++;
+        }
+
+        double promedio = (double)suma / notas.Length;
+
+        Console.WriteLine($"Promedio: {promedio}");
+        Console.WriteLine($"Aprobados: {aprobados}");
+        Console.WriteLine($"Reprobados: {reprobados}");
+    }
+}
---
+++ Proyecto Git/EjemploArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class EjemploArray
+{
+    static void Main()
+    {
+        string[] nombres = { "Ana", "Luis", "Carlos", "Maria" };
+        string buscado = "Carlos";
+
+        bool encontrado = false;
+
+        foreach (string nombre in nombres)
+        {
+            if (nombre == buscado)
+            {
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (encontrado)
+            Console.WriteLine("Elemento encontrado");
+        else
+            Console.WriteLine("Elemento no encontrado");
+    }
+}
